7be37be89df5c5e429e647b48c15ae452b889f8c
Sahil Jain <sahil.2441@gmail.com>
2015-01-31 21:20:48 +0530

    This is just a test

--- app/src/main/java/me/sahiljain/locationstat/MapsActivity.java
+++ app/src/main/java/me/sahiljain/locationstat/MapsActivity.java
@@ -10,6 +10,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapClickListener {
+    /**
+     * This is just a test
+     */
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
