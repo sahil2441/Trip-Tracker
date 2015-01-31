758161297ef9a5855b26eb07a3ae66bfebc984f5
Sahil Jain <sahil.2441@gmail.com>
2015-02-01 03:08:07 +0530

    Tap to select your home loction

--- app/src/main/java/me/sahiljain/locationstat/MapsActivity.java
+++ app/src/main/java/me/sahiljain/locationstat/MapsActivity.java
@@ -1,9 +1,12 @@
 package me.sahiljain.locationstat;
 
+import android.app.Dialog;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -12,6 +15,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapClickListener {
 
+    private Location location_home;
+    Dialog dialog;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
@@ -69,11 +74,28 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapCli
             if (location != null) {
                 double latitude = location.getLatitude();
                 double longitude = location.getLongitude();
-                mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Marker"));
+//                mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Marker"));
+                centerMapOnMYLocation(location);
             }
         } else {
             mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
         }
+    }
+
+    public void centerMapOnMYLocation(Location location) {
+        float zoom = 10;
+        mMap.setMyLocationEnabled(true);
+        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, zoom));
+        setupDialog();
+    }
+
+    private void setupDialog() {
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.popupview);
+        TextView textView = (TextView) dialog.findViewById(R.id.popup_text);
+        textView.setText("Tap screen to set your home location");
+        dialog.show();
     }
 
     @Override
