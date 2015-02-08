0748c3ba52cd7c3ab02b6d493d3d1452ac3d80e3
Sahil Jain <sahil.2441@gmail.com>
2015-02-08 22:10:00 +0530

    Added two new classes to implement notification feature. Successful implementation.

---
+++ app/src/main/java/me/sahiljain/locationstat/NotificationIntentService.java
@@ -0,0 +1,92 @@
+package me.sahiljain.locationstat;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+/**
+ * Created by sahil on 8/2/15.
+ */
+
+/**
+ * This {@code NotificationIntentService } does the actual handling of the GCM message.
+ * {@code NotificationReceiver} holds a
+ * partial wake lock for this service while the service does its work. When the
+ * service is finished, it calls {@code completeWakefulIntent()} to release the
+ * wake lock.
+ */
+
+public class NotificationIntentService extends IntentService {
+
+    public NotificationIntentService() {
+        super("NotificationIntentService");
+
+    }
+
+    public static final String TAG = "Notification Intent Service";
+
+    private NotificationManager notificationManager;
+
+    public static final int NOTIFICATION_ID = 1;
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging googleCloudMessaging = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+
+        String messageType = googleCloudMessaging.getMessageType(intent);
+        if (!extras.isEmpty()) { // has effect of unparcelling Bundle
+        /*
+             * Filter messages based on message type. Since it is likely that GCM will be
+             * extended in the future with new message types, just ignore any message types you're
+             * not interested in, or that you don't recognize.
+             */
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                sendNotification("Send Error: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+                sendNotification("Deleted messages on server " + extras.toString());
+            }
+            // If it's a regular GCM message, do some work.
+            else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                // This loop represents the service doing some work.
+                for (int i = 0; i < 5; i++) {
+                    Log.i(TAG, "Working......" + i + "/5 " + SystemClock.elapsedRealtime());
+                }
+                sendNotification("Received : " + extras.toString());
+                Log.i(TAG, "Received : " + extras.toString());
+            }
+            // Release the wake lock provided by the WakefulBroadcastReceiver.
+            NotificationReceiver.completeWakefulIntent(intent);
+        }
+    }
+    // Put the message into a notification and post it.
+    // This is just one simple example of what you might choose to do with
+    // a GCM message.
+
+    private void sendNotification(String message) {
+        notificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.
+                getActivity(this, 0, new Intent(this, MapsActivity.class), 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this).
+                setSmallIcon(R.drawable.homeiconsmall)
+                .setContentTitle("Location Stat Notification")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(message))
+                .setContentText(message);
+        builder.setContentIntent(contentIntent);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+}
