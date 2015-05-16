004438fd2a51962c270da3a551969c6f238b2f5b
Sahil Jain <sahil.2441@gmail.com>
2015-05-17 03:19:35 +0530

    Worked on Block Feature-- notifications can be blocked.

---
+++ app/src/main/java/me/sahiljain/tripTracker/main/NotificationDetailedActivity.java
@@ -0,0 +1,100 @@
+package me.sahiljain.tripTracker.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import me.sahiljain.tripTracker.R;
+import me.sahiljain.tripTracker.db.Persistence;
+
+/**
+ * Created by sahil on 6/5/15.
+ */
+public class NotificationDetailedActivity extends Activity {
+
+    private Button unblockUser;
+    private Button blockUser;
+    private TextView notification;
+    private TextView senderID;
+
+    private String senderIDFromIntent;
+    private String notificationFromIntent;
+    private Persistence persistence;
+    private SharedPreferences preferences;
+    private int currentColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.notification_detail_view);
+        unblockUser = (Button) findViewById(R.id.unblock_user);
+        blockUser = (Button) findViewById(R.id.block_user);
+        notification = (TextView) findViewById(R.id.notification_text);
+        senderID = (TextView) findViewById(R.id.sender_id);
+
+        Intent intent = getIntent();
+        notificationFromIntent = intent.getStringExtra(Constants.NOTIFICATION);
+        senderIDFromIntent = intent.getStringExtra(Constants.SENDER_ID);
+        notification.setText(intent.getStringExtra(Constants.TRIP_NAME));
+
+        preferences = this.getSharedPreferences(Constants.TRIP_TRACKER_SHARED_PREFERENCES, 0);
+        currentColor = preferences.getInt(Constants.CURRENT_COLOR, 0xFF666666);
+        notification.setBackgroundColor(currentColor);
+
+        blockUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blockUser(senderIDFromIntent);
+            }
+        });
+
+        unblockUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unblockUser(senderIDFromIntent);
+            }
+        });
+
+        /**
+         * Dim Background
+         */
+        WindowManager.LayoutParams windowManager = getWindow().getAttributes();
+        windowManager.dimAmount = 0.75f;
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+    }
+
+    private void unblockUser(String senderIDFromIntent) {
+        //TODO:
+        launchTabMainActivity();
+    }
+
+    private void blockUser(String senderIDFromIntent) {
+        //TODO:
+        launchTabMainActivity();
+    }
+
+
+    private void launchTabMainActivity() {
+        Intent intent = new Intent(this, TabMainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        this.finish();
+    }
+
+    /**
+     * Take care of popping the fragment back stack or finishing the activity
+     * as appropriate.
+     */
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+}
---
+++ app/src/main/java/me/sahiljain/tripTracker/notificationService/WakefulReceiver.java
@@ -0,0 +1,41 @@
+package me.sahiljain.tripTracker.notificationService;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by sahil on 8/2/15.
+ */
+
+/**
+ * This {@code WakefulBroadcastReceiver} takes care of creating and managing a
+ * partial wake lock for your app. It passes off the work of processing the GCM
+ * message to an {@code IntentService}, while ensuring that the device does not
+ * go back to sleep in the transition. The {@code IntentService} calls
+ * {@code GcmBroadcastReceiver.completeWakefulIntent()} when it is ready to
+ * release the wake lock.
+ */
+
+
+public class WakefulReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that NotificationIntentService will handle the intent.
+        ComponentName componentName = new ComponentName(context.getPackageName(),
+                NotificationIntentService.class.getName());
+
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, intent.setComponent(componentName));
+        setResultCode(Activity.RESULT_OK);
+
+        /**
+         * For Notification Service
+         * This ensures that service resumes on restarting the phone
+         */
+        Intent startNotificationServiceIntent = new Intent(context, NotificationSendingService.class);
+        context.startService(startNotificationServiceIntent);
+    }
+}
